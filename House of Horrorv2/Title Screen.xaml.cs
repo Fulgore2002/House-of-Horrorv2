f3e360be29cee88a8cb03549f92a09c6a28a8f55
Fulgore2002 <typryorhitchcock1@gmail.com>
2024-12-05 12:07:21 -0600

    Added a title screen

---
+++ House of Horrorv2/Title Screen.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace House_of_Horrorv2
+{
+    public partial class TitleScreen : Window
+    {
+        public TitleScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGame_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close(); // Close the title screen
+        }
+    }
+}
